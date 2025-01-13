4892b40b1580db0630ca602fc7266ae5fba6a454
{Rajan0402} <rajanyadav2873@gmail.com>
2025-01-13 19:43:17 +0530

    init

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [commonjs()],
+    },
+    commonjsOptions: {
+      include: ["src/**/*"],
+    },
+  },
+  optimizeDeps: {
+    include: ["api-contract/**/*"],
+  },
+});
