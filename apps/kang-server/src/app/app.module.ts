c07c1a7360b453ce1f506763f82d7a684fbbcf84
{Rajan0402} <rajanyadav2873@gmail.com>
2025-01-17 18:03:25 +0530

    app module moved to app folder

--- apps/kang-server/src/app/app.module.ts
+++ apps/kang-server/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DrizzleModule } from './drizzle/drizzle.module';
-import { UploadThingModule } from './upload/upload-thing.module';
-import { UploadFilesModule } from './upload-files/upload-files.module';
+import { DrizzleModule } from '../drizzle/drizzle.module';
+import { UploadThingModule } from '../upload/upload-thing.module';
+import { UploadFilesModule } from '../upload-files/upload-files.module';
 
 @Module({
   imports: [DrizzleModule, UploadThingModule, UploadFilesModule],
