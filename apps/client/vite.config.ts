8b69dfb58e6355991936871ed8cbd1b1077800ea
{Rajan0402} <rajanyadav2873@gmail.com>
2025-02-02 17:26:37 +0530

    ts, vite and axios config file changes and packages added

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "@rollup/plugin-commonjs";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +16,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["api-contract/**/*"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components/index"),
+    },
   },
 });
