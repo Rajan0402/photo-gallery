5306853839b27b123299dfa12bc1f2131165b839
{Rajan0402} <rajanyadav2873@gmail.com>
2025-05-25 22:15:24 +0530

    Upload and Home Page init

---
+++ apps/client/src/components/upload-btn/UploadBtn.tsx
@@ -0,0 +1,11 @@
+import { FiUpload } from "react-icons/fi";
+
+const UploadBtn = () => {
+  return (
+    <>
+      <FiUpload title="upload" size={"1.5rem"} />
+    </>
+  );
+};
+
+export default UploadBtn;
