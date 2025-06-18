7ab9b807fae82cc7b04597db118f0b4495ec06db
{Rajan0402} <rajanyadav2873@gmail.com>
2025-06-18 21:43:28 +0530

    upload btn implementation - upload working

--- apps/client/src/components/upload-btn/UploadBtn.tsx
+++ apps/client/src/components/upload-btn/UploadBtn.tsx
@@ -1,11 +1,42 @@
 import { FiUpload } from "react-icons/fi";
+import styles from "./UploadBtn.module.css";
+import useStartUpload from "@/hooks/useStartUpload";
+import { useState } from "react";
 
 const UploadBtn = () => {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const { result, isUploading, error } = useStartUpload(selectedFiles);
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files?.length < 1) {
+      console.log("No files selected");
+      return;
+    }
+
+    const filesArray = Array.from(e.target.files);
+    console.log("selected files---", filesArray);
+    setSelectedFiles(filesArray);
+
+    // console.log("uploaded files", result);
+    // TODO: persist result in state maybe?
+  };
+
   return (
     <>
-      <button>
+      <label
+        htmlFor="upload-button"
+        className={styles.upload_btn_label}
+        aria-disabled={isUploading}
+      >
         <FiUpload title="upload" size={"1.5rem"} />
-      </button>
+      </label>
+      <input
+        id="upload-button"
+        type="file"
+        onChange={handleChange}
+        multiple={true}
+        style={{ display: "none" }}
+      />
     </>
   );
 };
