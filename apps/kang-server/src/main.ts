b9b15c8f1c75af2956701f8b5028a87806b6d031
{Rajan0402} <rajanyadav2873@gmail.com>
2025-06-28 20:05:28 +0530

    test

--- apps/kang-server/src/main.ts
+++ apps/kang-server/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('first');
 
   app.use(cookieParser(process.env.COOKIE_SECRET));
 
